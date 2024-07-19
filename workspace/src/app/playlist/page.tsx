1c6ed23c5b2fd46e60dfe1569ffc72fa9296ad91
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-07-19 14:18:34 +0900

    playlist画面枠組み作成

--- workspace/src/app/playlist/page.tsx
+++ workspace/src/app/playlist/page.tsx
@@ -1,35 +1,122 @@
-import React from 'react'
-import "../../components/playlist-menu"
-import PlaylistMenu from '../../components/playlist-menu'
+"use client";
+import React, { useEffect } from 'react';
+import "../../components/playlist-menu";
+import PlaylistMenu from '../../components/playlist-menu';
 
-const page = () => {
+const Page: React.FC = () => {
+  useEffect(() => {
+    const copyUrlButton = document.getElementById('copy-url');
+
+    const handleCopyUrl = () => {
+      if (!copyUrlButton) return;
+      // data-urlの値を取得
+      const url = copyUrlButton.getAttribute('data-url');
+      if (!url) return;
+
+      // クリップボードにコピー
+      navigator.clipboard.writeText(url).then(() => {
+        // フラッシュメッセージ表示
+        const successMsg = document.querySelector('.success-msg') as HTMLElement;
+        if (successMsg) {
+          successMsg.style.display = 'block';
+          successMsg.style.opacity = '0';
+
+          // フェードイン
+          let opacity = 0;
+          const fadeIn = setInterval(() => {
+            if (opacity < 1) {
+              opacity += 0.1;
+              successMsg.style.opacity = opacity.toString();
+            } else {
+              clearInterval(fadeIn);
+              // フェードアウト
+              setTimeout(() => {
+                const fadeOut = setInterval(() => {
+                  if (opacity > 0) {
+                    opacity -= 0.1;
+                    successMsg.style.opacity = opacity.toString();
+                  } else {
+                    clearInterval(fadeOut);
+                    successMsg.style.display = 'none';
+                  }
+                }, 50);
+              }, 2000);
+            }
+          }, 50);
+        } else {
+          console.error('success-msg 要素が見つかりません');
+        }
+      }).catch(error => {
+        console.error('クリップボードへのコピーに失敗しました:', error);
+      });
+    };
+
+    if (copyUrlButton) {
+      copyUrlButton.addEventListener('click', handleCopyUrl);
+    }
+
+    // クリーンアップ
+    return () => {
+      if (copyUrlButton) {
+        copyUrlButton.removeEventListener('click', handleCopyUrl);
+      }
+    };
+  }, []);
+
   return (
-    <>
-        <div style={{position:"relative"}}>
-            {/* 本当は楽曲一覧画面に戻る */}
-            <a href='/'>
-                <img src="./arrow-image.png" style={{position:"absolute", top:"5vh",left:"30px"}}></img>
-            </a>
-            <a href="https://twitter.com/share?url={URL}" rel="nofollow noopener" target="_blank">
-                <img src='./export-image.png' style={{position:"absolute",top:"5vh",right:"30px"}}></img>
-            </a>
-            <div style={{
-                display:"flex",
-                alignItems:"center",
-                justifyContent:"center",
-                flexDirection:"column",
-                margin:"5px"
-                }}>
-                <img src='./sample/Himawari.jpg' style={{width:"30%", borderRadius:"10%",marginTop:"10vh"}} />
-                <p style={{fontWeight:"bold",fontSize:"30px"}}>プレイリスト名</p>
-            </div>
-            <div style={{marginTop:"10px"}}>
-                {/* 要調整 */}
-                <PlaylistMenu />
-            </div>
+    <div style={{ position: "relative" }}>
+      {/* 本当は楽曲一覧画面に戻る */}
+      <a href='/'>
+        <img src="./arrow-image.png" style={{ position: "absolute", top: "5vh", left: "30px" }} alt="戻る"></img>
+      </a>
+      <div className="dropdown dropdown-end" style={{ position: "absolute", top: "5vh", right: "30px" }}>
+        <div tabIndex={0}>
+          <img src='./export-image.png' alt="エクスポート" />
         </div>
-    </>
-  )
+        <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+          <li><a href="https://twitter.com">Xで共有</a></li>
+          <li><a href="/create">投稿する</a></li>
+          <li>
+            <button id="copy-url" data-url="ここにURL">リンクをコピー</button>
+            <div className="success-msg"
+              style={{
+                display: "none",
+                position: "fixed",
+                width: "300px",
+                height: "40px",
+                lineHeight: "25px",
+                background: "#0e7fff",
+                color: "#fff",
+                top: "100px",
+                left:"-100px",
+                
+                borderRadius: "5px",
+              }}
+            >クリップボードにコピーしました</div>
+          </li>
+        </ul>
+      </div>
+      <div style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        margin: "5px"
+      }}>
+        <img src='./sample/Himawari.jpg' style={{ width: "30%", borderRadius: "10%", marginTop: "10vh" }} alt="サンプル" />
+        <p style={{ fontWeight: "bold", fontSize: "30px" }}>プレイリスト名</p>
+      </div>
+      <div style={{ marginTop: "10px" }}>
+        {/* ここリストで管理したい */}
+        <PlaylistMenu />
+        <PlaylistMenu />
+        <PlaylistMenu />
+        <div style={{ width: "100vw", height: 1, background: "gray" }} />
+      </div>
+      {/* 編集画面に飛ぶリンク貼る */}
+      <a href="/edit" className="link text-blue-500 hover:text-blue-700" style={{ marginLeft: "30px" }}>プレイリストを編集する</a>
+    </div>
+  );
 }
 
-export default page
+export default Page;
--- workspace/src/components/playlist-menu.tsx
+++ workspace/src/components/playlist-menu.tsx
@@ -4,11 +4,23 @@ const PlaylistMenu = () => {
   return (
     <>
     <div style={{width:"100vw",height:1,background:"gray"}} />
-    <div style={{position:"relative", display:"flex", justifyContent:"center",alignItems:"center"}}>
-        <img src='./sample/Himawari.jpg' style={{width:"50px",position:"absolute",left:"10vw",marginTop:"30px",borderRadius:"10%"}} />
-        <p style={{marginTop:"30px"}}>曲名</p>
-        <div className="dropdown dropdown-end" style={{position:"absolute",right:"10vh",marginTop:"30px"}} >
-            <div tabIndex={0} role="button" className="d"><img src='./menu-button.png' /></div>
+    <div 
+    style={{
+       display:"flex", 
+       justifyContent:"space-between",
+       alignItems:"center",
+       margin:"10px 0px 10px 0px"
+       }}>
+        <img src='./sample/Himawari.jpg' alt='曲の画像' 
+        style={{
+          width:"50px",
+          borderRadius:"10%",
+          marginLeft:"10%"
+          }} />
+        <p>曲名</p>
+        <div className="dropdown dropdown-end" 
+        style={{marginRight:"10%"}} >
+            <div tabIndex={0} role="button"><img src='./menu-button.png' /></div>
             <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
                 <li><a>情報を見る</a></li>
                 {/* 曲の情報ページ作った方がいいかも */}
